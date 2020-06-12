ea04a776394e4dd62080c7e644a93c1825cb9b61
Morales7nl21 <thedjdave98@gmail.com>
2020-06-12 02:13:49 -0500

    Se modifico para cuando se incerten datos el id que falte se le asocie de no faltar se le pone un numero de  id mayor al ultimo

--- admin/ajax/addMateriaDetails.php
+++ admin/ajax/addMateriaDetails.php
@@ -1,26 +1,34 @@
 <?php
-    if(isset($_POST['idMateria']) && isset($_POST['nombreA']) && isset($_POST['profesor'])
-    && isset($_POST['cupo']) && isset($_POST['salon']) && isset($_POST['horario']))
-	{
-		// include Database connection file 
-		include("db_connection.php");
+if (
+	isset($_POST['idMateria']) && isset($_POST['nombreA']) && isset($_POST['profesor'])
+	&& isset($_POST['cupo']) && isset($_POST['salon']) && isset($_POST['horario'])
+) {
+	// include Database connection file 
+	include("db_connection.php");
 
-		// get values 
-		$idMateria = $_POST['idMateria'];
-		$nombreA = $_POST['nombreA'];
-		$profesor = $_POST['profesor'];
-        $cupo = $_POST['cupo'];
-		$salon = $_POST['salon'];
-		$horario = $_POST['horario'];
-        $sql="SELECT count(idMateria) as total from materias_disponibles";
-        $result2=mysqli_query($con,$sql);
-        $mostrar = mysqli_fetch_assoc($result2);
-        $var=(($mostrar['total'])+1);
-        echo "$var";
-		$query = "INSERT INTO `materias_disponibles` (`idMateria`, `materia`, `profesor`, `cupo`, `salon`, `horario`) VALUES('$var', '$nombreA', '$profesor', '$cupo', '$salon', '$horario')";
-		if (!$result = mysqli_query($con, $query)) {
-	        exit(mysqli_error($con));
-	    }
-	    echo "1 Record Added!";
+	// get values 
+	$idMateria = $_POST['idMateria'];
+	$nombreA = $_POST['nombreA'];
+	$profesor = $_POST['profesor'];
+	$cupo = $_POST['cupo'];
+	$salon = $_POST['salon'];
+	$horario = $_POST['horario'];
+	$sql = "SELECT count(idMateria) as total from materias_disponibles";
+	$result2 = mysqli_query($con, $sql);
+	$mostrar = mysqli_fetch_assoc($result2);
+
+	$bool=TRUE;	
+	$var=1;
+
+	while($bool == TRUE){
+
+	$query = "INSERT INTO `materias_disponibles` (`idMateria`, `materia`, `profesor`, `cupo`, `salon`, `horario`) VALUES('$var', '$nombreA', '$profesor', '$cupo', '$salon', '$horario')";
+	if (!$result = mysqli_query($con, $query)) {
+		
+	}else{
+		$bool=FALSE;
+		echo "1 Record Added!";
 	}
-?>
+	$var += 1;
+	}
+}
